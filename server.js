01754d6c445b3ee74532db4834d5d0d2f4bd4fb3
GTatertots <root@DESKTOP-OVBT6VQ.localdomain>
2024-04-22 12:49:52 -0600

    fixed typo

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
     cookie: { 
         secure: true,
         sameSite: 'None'
